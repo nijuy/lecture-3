8c381d4fd593108200ac9b8ecf7e34ff56889698
nijuy <youjin6325@naver.com>
2024-10-22 00:35:51 +0900

    refactor: `BannerVideo` 텍스트에 페이드인 애니메이션 추가
    - 글꼴 로딩에 따라 적용

--- src/components/BannerVideo.js
+++ src/components/BannerVideo.js
@@ -1,8 +1,21 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import video from '../assets/banner-video.mp4'
 import video_webm from '../assets/banner-video.webm'
+import FontFaceObserver from 'fontfaceobserver';
+
+const font = new FontFaceObserver('BMYEONSUNG');
 
 function BannerVideo() {
+	const [isFontLoaded, setIsFontLoaded] = useState(false);
+
+
+	useEffect(() => {
+		font.load(null, 20000).then(() => {
+			console.log('폰트 로딩이 완료되었습니다.');
+			setIsFontLoaded(true);
+		});
+	}, [])
+
 	return (
 		<div className="BannerVideo w-full h-screen overflow-hidden relative bg-texture">
 			<div className="absolute h-screen w-full left-1/2">
@@ -11,7 +24,7 @@ function BannerVideo() {
 				  <source src={video_webm} type='video/webm'/>
 				</video>
 			</div>
-			<div className="w-full h-full flex justify-center items-center">
+			<div className="w-full h-full flex justify-center items-center" style={{ opacity: isFontLoaded ? 1 : 0, transition: 'opacity 0.3s ease'}}>
 				<div className="text-white text-center">
 					<div className="text-6xl leading-none font-semibold">KEEP</div>
 					<div className="text-6xl leading-none font-semibold">CALM</div>
