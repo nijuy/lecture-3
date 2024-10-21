9cf7f28978558cc989db71c9ea887943d72717e7
nijuy <youjin6325@naver.com>
2024-10-22 00:14:45 +0900

    refactor: `MainPage`의 img를 picture로 감싸기

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -26,7 +26,8 @@ function MainPage(props) {
 		const callback = (entries, observer) => {
 			entries.forEach((entry) => {
 				if(entry.isIntersecting) {
-					console.log('is intersecting', entry.target.dataset.src);
+					const sourceEl = entry.target.previousSibling;
+					sourceEl.srcset = sourceEl.dataset.srcset;
 					entry.target.src = entry.target.dataset.src;
 					observer.unobserve(entry.target);
 				}
@@ -55,7 +56,10 @@ function MainPage(props) {
 				<TwoColumns
 					bgColor={'#f4f4f4'}
 					columns={[
-						<img data-src={main_items} ref={imgRef} />,
+						<picture>
+							<source data-srcset={main_items_webp} type='image/webp' />
+							<img data-src={main_items} ref={imgRef} alt='' />
+						</picture>,
 						<Meta
 							title={'Items'}
 							content={'롱보드는 기본적으로 데크가 크기 때문에 입맛에 따라 정말 여러가지로 변형된 형태가 나올수 있습니다. 실제로 데크마다 가지는 모양, 재질, 무게는 천차만별인데, 본인의 라이딩 스타일에 맞춰 롱보드를 구매하시는게 좋습니다.'}
@@ -71,14 +75,20 @@ function MainPage(props) {
 							content={'롱보드는 데크, 트럭, 휠, 킹핀, 베어링 등 여러 부품들로 구성됩니다. 롱보드를 타다보면 조금씩 고장나는 부품이 있기 마련인데, 이럴때를 위해 롱보들의 부품들에 대해서 알고 있으면 큰 도움이 됩니다.'}
 							btnLink={'/part'}
 						/>,
-						<img data-src={main_parts} ref={imgRef2} />
+						<picture>
+							<source data-srcset={main_parts_webp} type='image/webp' />
+							<img data-src={main_parts} ref={imgRef2} alt='' />
+						</picture>
 					]}
 					mobileReverse={true}
 				/>
 				<TwoColumns
 					bgColor={'#f4f4f4'}
 					columns={[
-						<img data-src={main_styles} ref={imgRef3} />,
+						<picture>
+							<source data-srcset={main_styles_webp} type='image/webp' />
+							<img data-src={main_styles} ref={imgRef3} alt='' />
+						</picture>,
 						<Meta
 							title={'Riding Styles'}
 							content={'롱보드 라이딩 스타일에는 크게 프리스타일, 다운힐, 프리라이딩, 댄싱이 있습니다. 보통 롱보드는 라이딩 스타일에 따라 데크의 모양이 조금씩 달라집니다. 많은 롱보드 매니아들이 각 쓰임새에 맞는 보드들을 소유하고 있습니다.'}
