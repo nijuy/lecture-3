d42e860657d1310fe8efeb0880006b08c844765a
nijuy <youjin6325@naver.com>
2024-10-22 00:10:46 +0900

    refactor: `Card`의 img를 picture로 감싸기
    - webp 지원이 안 될 경우를 대비

--- src/components/Card.js
+++ src/components/Card.js
@@ -7,8 +7,12 @@ function Card(props) {
 		const callback = (entries, observer) => {
 			entries.forEach((entry) => {
 				if(entry.isIntersecting) {
+					const target = entry.target;
+					const prevSibling = target.previousSibling;
+
 					console.log('is intersecting', entry.target.dataset.src);
-					entry.target.src = entry.target.dataset.src;
+					target.src = target.dataset.src;
+					prevSibling.srcset = prevSibling.dataset.srcset
 					observer.unobserve(entry.target);
 				}
 			})
@@ -22,7 +26,10 @@ function Card(props) {
 
 	return (
 		<div className="Card text-center">
-			<img data-src={props.image} ref={imgRef} />
+			<picture>
+				<source data-srcset={props.webp} type='image/webp' />
+				<img data-src={props.image} ref={imgRef} />
+			</picture>
 			<div className="p-5 font-semibold text-gray-700 text-xl md:text-lg lg:text-xl keep-all">
 				{props.children}
 			</div>
